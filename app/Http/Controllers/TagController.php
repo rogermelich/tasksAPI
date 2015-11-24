218e0b5a864098a7dfe23e4b756c92778a5609db
rogermelich <rogermelich@gmail.com>
2015-11-24 21:28:48 +0100

    Add return Tag::all(); to TagController

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use phpDocumentor\Reflection\DocBlock\Tag;
 
 class TagController extends Controller
 {
@@ -16,7 +17,7 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+        return Tag::all();
     }
 
     /**
